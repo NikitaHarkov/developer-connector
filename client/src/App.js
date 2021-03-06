f9792d66574a39e0cab623c0777d6b244063c0ea
«Nikita <mrnikita.harkov@gmail.com>
2021-03-06 21:23:44 +0200

    Setup react routing and login,register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Landing, Navbar, Login, Register } from './components';
+import { Error } from './pages';
 import './App.css';
-import { Landing, Navbar } from './components';
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Landing />
-    </>
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route path='*'>
+            <Error />
+          </Route>
+        </Switch>
+      </section>
+    </Router>
   );
 }
 
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,6 @@
 import Landing from './layout/Landing';
 import Navbar from './layout/Navbar';
+import Login from './auth/Login';
+import Register from './auth/Register';
 
-export { Landing, Navbar };
+export { Landing, Navbar, Login, Register };
