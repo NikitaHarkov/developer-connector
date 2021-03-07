61b41b7a315394649362f7e9dc87142f7a0d824c
«Nikita <mrnikita.harkov@gmail.com>
2021-03-07 10:47:47 +0200

    Setup redux in app

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,27 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Landing, Navbar, Login, Register } from './components';
 import { Error } from './pages';
 import './App.css';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-          <Route path='*'>
-            <Error />
-          </Route>
-        </Switch>
-      </section>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+            <Route path='*'>
+              <Error />
+            </Route>
+          </Switch>
+        </section>
+      </Router>
+    </Provider>
   );
 }
 
