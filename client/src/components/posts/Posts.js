a19f7bfa99db3bb40bafaa8e628981885a7cab77
«Nikita <mrnikita.harkov@gmail.com>
2021-03-13 21:43:11 +0200

    Create posts components

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,14 +2,31 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/postAction';
-//import Loading from '../layout/Loading';
+import Loading from '../layout/Loading';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return <div></div>;
+  if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <>
+        <h1 className='large text-primary'>Posts</h1>
+        <p className='lead'>
+          <i className='fas fa-user'></i> Welcome to the community
+        </p>
+        <div className='posts'>
+          {posts.map(post => (
+            <PostItem key={post._id} {...post} />
+          ))}
+        </div>
+      </>
+    );
+  }
 };
 
 Posts.propTypes = {
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -2,7 +2,7 @@ export const formatDate = date => {
   try {
     return new Intl.DateTimeFormat('et-EE', {
       year: 'numeric',
-      month: 'long',
+      month: '2-digit',
       day: '2-digit',
     }).format(new Date(date));
   } catch (error) {
